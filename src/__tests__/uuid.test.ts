aaefd5d1d8e09814d526c9ba3de17aa6a5527df8
Edward Kim <edward@cyberbird.co>
2025-05-31 02:20:00 -0500

    feat: adding UUID utility

---
+++ src/__tests__/uuid.test.ts
@@ -0,0 +1,11 @@
+import isString from '../isString';
+import uuid from '../uuid';
+
+describe('uuid', () => {
+	it('should generate a UUID', () => {
+		const id = uuid();
+
+		expect(isString(id)).toBe(true);
+		expect(id).toHaveLength(36);
+	});
+});
