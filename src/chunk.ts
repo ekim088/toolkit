5254b5cf6230216d82b87b2675828dc8f11357b5
Edward Kim <edward@cyberbird.co>
2026-05-26 22:01:24 -0500

    feat: adding chunk utility (#10)

---
+++ src/chunk.ts
@@ -0,0 +1,20 @@
+/**
+ * Chunks an array into sub-arrays of at most `chunkSize` items.
+ * @template T
+ * @param {T[]} items The array to chunk.
+ * @param {number} chunkSize The max number of items per chunk.
+ * @returns {T[][]} The chunked array.
+ */
+export function chunk<T>(items: readonly T[], chunkSize: number): T[][] {
+	const result: T[][] = [];
+
+	if (chunkSize < 1) {
+		return [];
+	}
+
+	for (let i = 0; i < items.length; i += chunkSize) {
+		result.push([...items.slice(i, i + chunkSize)]);
+	}
+
+	return result;
+}
