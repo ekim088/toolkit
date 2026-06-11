62f220ffdc5f93a088080aff921c3d852429ee3e
Edward Kim <edward@cyberbird.co>
2026-06-11 17:51:21 -0500

    style: formatting YAML files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,10 +3,6 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-	'*.(j|t)s': [
-		'yarn exec eslint --fix',
-		'yarn exec prettier --write',
-		'yarn test:pre-commit',
-	],
-	'*.(json|md|*rc)': ['yarn exec prettier --write'],
+	'!(*.(j|t)s)': 'prettier --write --ignore-unknown',
+	'*.(j|t)s': ['eslint --fix', 'prettier --write', 'yarn test:pre-commit'],
 };
